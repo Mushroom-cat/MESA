431400739a254feb0e314c4bbb394c20cafe0809
Mushroom-cat <58413436+Mushroom-cat@users.noreply.github.com>
2024-09-07 00:17:03 +0800

    Delete dbook directory

--- dbook/1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for((i=11;i<=41;i+=10));
-do
-python main.py --device=cuda --dataset=dbook --train_dir=default --state_dict_path='dbook_default/bias10.pth' --inference_only=true --maxlen=200 --template train_bert --least_inter_num $i --most_inter_num $(expr $i + 10);
-done
